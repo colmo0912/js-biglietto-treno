06bf8ee782ad90992d1d16b333b6b814457a7695
colmo0912 <colmo0912@gmail.com>
2021-04-08 15:51:02 +0200

    imposto gli if / else

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
 var numKm = parseInt (prompt("Quanti KM devi percorrere?"));
-console.log(numKm);
+// console.log(numKm);
 var anniPasseggero = parseInt (prompt("Quanti anni ha il passeggero?"));
-console.log(anniPasseggero);
+// console.log(anniPasseggero);
 
 var prezzoAlKm = 0.21;
 
 console.log(parseFloat(prezzoAlKm));
 
 var prezzoBiglietto = numKm * prezzoAlKm;
-console.log(prezzoBiglietto)
+console.log(prezzoBiglietto);
+
+var passeggeroMinorenne = parseInt(17);
+// console.log(parseInt(passeggeroMinorenne));
+
+// var passeggeroMaggiorenne 
+// console.log(parseInt(passeggeroMaggiorenne));
+
+var passeggeroOver = parseInt(65);
+// console.log(parseInt(passeggeroOver));
+
+
+
+if(anniPasseggero<=passeggeroMinorenne){
+
+    console.log("il passeggero è minorenne")
+}
+if(anniPasseggero>=passeggeroOver){
+
+    console.log("il passeggero ha più di 65 anni")
+}else {
+
+    console.log("il passeggero non ha sconti sul biglietto")
+}
