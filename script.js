643b849af4929aa790f470104b30b444cfafaf81
colmo0912 <colmo0912@gmail.com>
2021-04-08 14:53:38 +0200

    aggiungo variabili km ed età con i prompt

---
+++ script.js
@@ -0,0 +1,4 @@
+var numKm = parseInt (prompt("Quanti KM devi percorrere?"));
+console.log(numKm);
+var anniPasseggero = parseInt (prompt("Quanti anni ha il passeggero?"));
+console.log(anniPasseggero);
