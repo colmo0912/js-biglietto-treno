f732e722262dc11370a0cc03932308f519e0f5e1
colmo0912 <colmo0912@gmail.com>
2021-04-08 15:07:50 +0200

    dichiaro variabile prezzoAlKm

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ var numKm = parseInt (prompt("Quanti KM devi percorrere?"));
 console.log(numKm);
 var anniPasseggero = parseInt (prompt("Quanti anni ha il passeggero?"));
 console.log(anniPasseggero);
+
+var prezzoAlKm = 0.21;
+
+console.log(parseFloat(prezzoAlKm))
